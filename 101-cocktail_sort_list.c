ba2c3bd58ce61586a7ddb20593e481433717d3c7
Your Name <you@example.com>
2023-07-26 23:04:56 -0700

    update

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,20 +9,21 @@
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
-    listint_t *back = node->prev, *current = node;
+	listint_t *back = node->prev, *current = node;
 
-    back->next = current->next;
-    if (current->next)
-        current->next->prev = back;
-    current->next = back;
-    current->prev = back->prev;
-    back->prev = current;
-    if (current->prev)
-        current->prev->next = current;
-    else
-        *list = current;
+	back->next = current->next;
 
-    return (current);
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
 
 /**
@@ -32,42 +33,41 @@ listint_t *swap_node(listint_t *node, listint_t **list)
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *node;
-    int swap_done = 1;
+	listint_t *node;
+	int swap_done = 1;
 
-    if (list == NULL || (*list) == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
 
-    node = *list;
-    while (swap_done == 1)
-    {
-        swap_done = 0;
-        while (node->next)
-        {
-            if (node->n > node->next->n)
-            {
-                node = swap_node(node->next, list);
-                swap_done = 1;
-                print_list(*list);
-            }
-            else
-                node = node->next;
-        }
+	node = *list;
 
-        if (swap_done == 0)
-            break;
-
-        swap_done = 0;
-        while (node->prev)
-        {
-            if (node->n < node->prev->n)
-            {
-                node = swap_node(node, list);
-                swap_done = 1;
-                print_list(*list);
-            }
-            else
-                node = node->prev;
-        }
-    }
+	while (swap_done == 1)
+	{
+		swap_done = 0;
+		while (node->next)
+		{
+			if (node->n > node->next->n)
+			{
+				node = swap_node(node->next, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->next;
+		}
+		if (swap_done == 0)
+			break;
+		swap_done = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				node = swap_node(node, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->prev;
+		}
+	}
 }
